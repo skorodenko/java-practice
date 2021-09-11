40392a02079c71143263c05338ef09af5fe34169
Dima Skorodenko <mskorodenko@gmail.com>
2021-09-11 22:13:43 +0300

    Lab_2, Step 5

--- Lab_2/src/main/java/StringCalculator.java
+++ Lab_2/src/main/java/StringCalculator.java
@@ -1,9 +1,10 @@
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringCalculator {
 
-    public int Add(String numbers) {
+    public int Add(String numbers) throws Exception {
         String delimiter = ",";
 
         Pattern pattern = Pattern.compile("//(.)\n");
@@ -16,13 +17,24 @@ public class StringCalculator {
         String[] str_arr = numbers.split(String.format("[%s\n]", delimiter));
 
         int sum = 0;
+        ArrayList<Integer> negativeError = new ArrayList<Integer>();
         for(String number: str_arr) {
             try {
-                sum += Integer.parseInt(number);
+                int num = Integer.parseInt(number);
+
+                if(num < 0) {
+                    negativeError.add(num);
+                }
+
+                sum += num;
             }
             catch(NumberFormatException e) {
                 continue;
             }
+        }
+
+        if(!negativeError.isEmpty()) {
+            throw new Exception("Negatives are not allowed: " + negativeError);
         }
 
         return sum;
--- Lab_2/src/test/java/StringCalculatorTest.java
+++ Lab_2/src/test/java/StringCalculatorTest.java
@@ -15,50 +15,75 @@ public class StringCalculatorTest {
 
     // Step 1 tests
     @Test
-    public void gives_null_for_empty_string() {
+    public void gives_null_for_empty_string() throws Exception {
         assertEquals(0, calculator.Add(""));
     }
 
     @Test
-    public void equals_to_one() {
+    public void equals_to_one() throws Exception {
         assertEquals(1, calculator.Add("1"));
     }
 
     @Test
-    public void equals_to_six() {
+    public void equals_to_six() throws Exception {
         assertEquals(6, calculator.Add("1,2,3"));
     }
 
     // Step 2 tests
     @Test
-    public void equals_to_seven() {
-        assertEquals(7, calculator.Add("1,2,-2,6"));
+    public void equals_to_seven() throws Exception {
+        assertEquals(7, calculator.Add("1,2,1,3"));
     }
 
     @Test
-    public void equals_to_fifteen() {
+    public void equals_to_fifteen() throws Exception {
         assertEquals(15, calculator.Add("1,2,3,4,5"));
     }
 
     // Step 3 tests
     @Test
-    public void equals_to_three() {
+    public void equals_to_three() throws Exception {
         assertEquals(3, calculator.Add("1,1\n1"));
     }
 
     @Test
-    public void equals_to_null_with_newline() {
+    public void equals_to_null_with_newline() throws Exception {
         assertEquals(0, calculator.Add("\n"));
     }
 
     // Step 4 tests
     @Test
-    public void semicolon_equal_to_four() {
+    public void semicolon_equal_to_four() throws Exception {
         assertEquals(4, calculator.Add("//;\n1;2\n1"));
     }
 
     @Test
-    public void dot_equal_to_seven() {
+    public void dot_equal_to_seven() throws Exception {
         assertEquals(7, calculator.Add("//.\n1.2\n3.1"));
+    }
+
+    // Step 5 tests
+    @Test
+    public void exception_thrown_one_negative() {
+        boolean thrown = false;
+        try {
+            calculator.Add("1,-1");
+        } catch (Exception e) {
+            assertEquals("Negatives are not allowed: [-1]", e.getMessage());
+            thrown = true;
+        }
+        assertTrue(thrown);
+    }
+
+    @Test
+    public void exception_thrown_three_negatives() {
+        boolean thrown = false;
+        try {
+            calculator.Add("1,-1,-2,-3");
+        } catch (Exception e) {
+            assertEquals("Negatives are not allowed: [-1, -2, -3]", e.getMessage());
+            thrown = true;
+        }
+        assertTrue(thrown);
     }
 }
