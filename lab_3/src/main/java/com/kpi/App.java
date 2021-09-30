e7247de1f6ccfd2838fe343ad11bb0de6f6e7e59
Dima Skorodenko <mskorodenko@gmail.com>
2021-09-30 17:31:37 +0300

    Changed copy method + added clearer test route.

--- lab_3/src/main/java/com/kpi/App.java
+++ lab_3/src/main/java/com/kpi/App.java
@@ -4,9 +4,65 @@ public class App
 {
     public static void main( String[] args )
     {
-        Matrix<Integer> v = Matrix.randomColumnVector(4);
-        v.set(0, 101);
+        // Task 1 incapsulated Matrix
 
-        System.out.println(v);
+        /*
+        // Task 2 Constructors + copy
+        Matrix<Integer> c1 = new Matrix<>();
+        System.out.println(c1 + "\n");
+
+        Matrix<Integer> c2 = new Matrix<>(3,2);
+        System.out.println(c2 + "\n");
+
+        Matrix<Integer> c3 = new Matrix<>(c2);
+        c2.set(0,0,1);
+        c3.set(0,0,9);
+        System.out.println(c2 + "\n");
+        System.out.println(c3 + "\n");
+        */
+
+        /* 
+        // Task 3 setter
+        Matrix<Integer> m = new Matrix<>(2,2);
+        m.set(0,0,1);
+        m.set(0,1,2);
+        m.set(1,0,3);
+        System.out.println(m + "\n");
+        */
+
+        /*
+        // Task 4 getters
+        Matrix<Integer> m = Matrix.randomMatrix(3, 4);
+        System.out.println(m + "\n");
+        System.out.println("Element at 0,0: " + m.get(0,0) + "\n");
+        System.out.println("2nd column of matrix: " + m.getColumn(1) + "\n");
+        System.out.println("3rd row of matrix: " + m.getRow(2));
+        */
+
+        /*
+        // Task 5 getSize
+        Matrix<Integer> m = new Matrix<>(13,23);
+        System.out.println(m.getSize());
+        */
+
+        // Task 6 equals & hashCode
+
+        // Task 7 immutable Matrix
+        
+        /*
+        // Task 8 Diagonal Matrix
+        Matrix<Integer> cv = Matrix.randomColumnVector(4);
+        System.out.println(cv + "\n");
+        Matrix<Integer> m = Matrix.diagonalMatrix(cv);
+        System.out.println(m);
+        */
+        
+        /*
+        // Task 9 Triangular Matrix
+        Matrix<Integer> m = Matrix.randomMatrix(4, 4);
+        System.out.println(m + "\n");
+        m.upperTriangular();
+        System.out.println(m);
+        */
     }
 }
